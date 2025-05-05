2b226be6fe6649281a724acb68463d649643acf5
Christopher Williams <williamstophef@gmail.com>
2025-05-05 13:12:37 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿static int SumOfDigit(int x)
+{
+    int sum = 0;
+    while (x > 0)
+    {
+        int digit = x % 10;
+        sum += digit;
+        x /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine(SumOfDigit(1234));
+Console.WriteLine(SumOfDigit(57394857));
+Console.WriteLine(SumOfDigit(126454));
+
